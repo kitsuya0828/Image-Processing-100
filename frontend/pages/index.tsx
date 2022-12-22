56989739401be2743e9c326296b2deac419190ca
Kitsuya0828 <kitsuyaazuma@gmail.com>
2022-12-23 03:01:28 +0900

    [remove] css files

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-import { App } from '../components/App'
-
-const inter = Inter({ subsets: ['latin'] })
+import { App } from "../components/App";
 
 export default function Home() {
   return (
     <>
       <App />
     </>
-  )
+  );
 }
