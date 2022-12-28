b29ef597b16f6c50492bdfc7e26528a239cb06e8
Kitsuya0828 <kitsuyaazuma@gmail.com>
2022-12-29 02:08:47 +0900

    [add] nested navbar

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,126 +1,26 @@
-import { ActionIcon, Button, Loader, TextInput } from '@mantine/core';
-import axios from 'axios';
-import React, { useState } from 'react'
-import { IconSquareRoundedX } from '@tabler/icons';
-import { ImageDropzone } from './ImageDropzone';
+import { ActionIcon, Button, Loader, TextInput } from "@mantine/core";
+import axios from "axios";
+import React, { useState } from "react";
+import { IconSquareRoundedX } from "@tabler/icons";
+import { ImageDropzone } from "./ImageDropzone";
+
+import { NavbarNested } from "./pages/Navbar";
+import { AppShell } from "@mantine/core";
 
 export const App = () => {
   return (
-    <div>
-        <ImageDropzone />
-    </div>
-  )
-}
-
-// const CommentForm = () => {
-// 	const [isCommentSending, setIsCommentSending] = useState(false);
-// 	const [image, setImage] = useState<File>(null);
-// 	const maxImagesUpload = 4; // 画像を最大4枚まで選択・アップロード
-// 	const [commentText, setCommentText] = useState<string>("");
-// 	const inputId = Math.random().toString(32).substring(2);
-
-// 	const handleOnSubmit = async (e: React.SyntheticEvent): Promise<void> => {
-// 		e.preventDefault();
-// 		setIsCommentSending(true);
-
-// 		const target = e.target as typeof e.target & {
-// 			comment: { value: string };
-// 		};
-
-// 		const data = new FormData();
-// 		data.append("images[]", image);
-// 		data.append("comment", target.comment?.value || "");
-// 		const postedComment = await axios.post(
-// 			'/api/v1/comments',
-// 			data
-// 		);
-
-//     setIsCommentSending(false);
-// 	};
-
-// 	const handleOnAddImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-// 		if (!e.target.files) return;
-// 		setImages([...images, ...e.target.files]);
-// 	};
-
-// 	const handleOnRemoveImage = (index: number) => {
-//     // 選択した画像は削除可能
-// 		const newImages = [...images];
-// 		newImages.splice(index, 1);
-// 		setImages(newImages);
-// 	};
-
-// 	return (
-// 		<form action="" onSubmit={(e) => handleOnSubmit(e)}>
-// 			<TextInput
-// 				name="comment"
-// 				value={commentText}
-// 				placeholder="コメントを書く"
-// 				disabled={isCommentSending}
-// 				onChange={(e) => setCommentText(e.target.value)}
-// 			/>
-//       {/* 1つのボタンで画像を選択する */}
-// 			<label htmlFor={inputId}>
-// 				<Button
-// 					disabled={images.length >= maxImagesUpload}
-// 					component="span"
-// 				>
-// 					画像追加
-// 				</Button>
-// 				<input
-// 					id={inputId}
-// 					type="file"
-// 					multiple
-// 					accept="image/*,.png,.jpg,.jpeg,.gif"
-// 					onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-// 						handleOnAddImage(e)
-// 					}
-// 					style={{ display: "none" }}
-// 				/>
-// 			</label>
-//       {/* 画像を選択したら選択中のすべての画像のプレビューを表示 */}
-// 			{images.map((image, i) => (
-// 				<div
-// 					key={i}
-// 					style={{
-// 						position: "relative",
-// 						width: "40%",
-// 					}}
-// 				>
-// 					<ActionIcon
-// 						aria-label="delete image"
-// 						style={{
-// 							position: "absolute",
-// 							top: 10,
-// 							left: 10,
-// 							color: "#aaa",
-// 						}}
-// 						onClick={() => handleOnRemoveImage(i)}
-// 					>
-// 						<IconSquareRoundedX />
-// 					</ActionIcon>
-// 					<img
-// 						src={URL.createObjectURL(image)}
-// 						style={{
-// 							width: "100%"
-// 						}}
-// 					/>
-// 				</div>
-// 			))}
-//       <br />
-//       <br />
-// 			{isCommentSending ? (
-// 				<Loader />
-// 			) : (
-// 				<Button
-// 					type="submit"
-// 					disabled={!commentText}
-// 				>
-// 					投稿
-// 				</Button>
-// 			)}
-// 		</form>
-// 	);
-// };
-
-// export default CommentForm;
+    <AppShell
+      padding="md"
+      navbar={<NavbarNested />}
+      // header={<Header height={60} p="xs">{/* Header content */}</Header>}
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      })}
+    >
+      <ImageDropzone />
+    </AppShell>
+  );
+};
---
+++ frontend/components/pages/Navbar.tsx
@@ -0,0 +1,159 @@
+import {
+  ActionIcon,
+  Anchor,
+  Code,
+  createStyles,
+  Group,
+  Navbar,
+  ScrollArea,
+  Title,
+  UnstyledButton,
+  Text,
+  Button,
+} from "@mantine/core";
+import {
+  IconNotes,
+  IconCalendarStats,
+  IconGauge,
+  IconPresentationAnalytics,
+  IconFileAnalytics,
+  IconAdjustments,
+  IconLock,
+  IconBrandGithub,
+} from "@tabler/icons";
+import { LinksGroup } from "./NavbarLinksGroup";
+
+const mockdata = [
+  { label: "問題1-10", icon: IconGauge },
+  {
+    label: "問題11-20",
+    icon: IconNotes,
+    // initiallyOpened: true,
+    links: [
+      { label: "Overview", link: "/" },
+      { label: "Forecasts", link: "/" },
+      { label: "Outlook", link: "/" },
+      { label: "Real time", link: "/" },
+    ],
+  },
+  {
+    label: "問題21-30",
+    icon: IconCalendarStats,
+    links: [
+      { label: "Upcoming releases", link: "/" },
+      { label: "Previous releases", link: "/" },
+      { label: "Releases schedule", link: "/" },
+    ],
+  },
+  { label: "問題31-40", icon: IconPresentationAnalytics },
+  { label: "問題41-50", icon: IconFileAnalytics },
+  { label: "問題51-60", icon: IconAdjustments },
+  {
+    label: "問題61-70",
+    icon: IconLock,
+    links: [
+      { label: "Enable 2FA", link: "/" },
+      { label: "Change password", link: "/" },
+      { label: "Recovery codes", link: "/" },
+    ],
+  },
+  {
+    label: "問題71-80",
+    icon: IconLock,
+    links: [
+      { label: "Enable 2FA", link: "/" },
+      { label: "Change password", link: "/" },
+      { label: "Recovery codes", link: "/" },
+    ],
+  },
+  { label: "問題81-90", icon: IconFileAnalytics },
+  { label: "問題91-100", icon: IconAdjustments },
+];
+
+const useStyles = createStyles((theme) => ({
+  navbar: {
+    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
+    paddingBottom: 0,
+  },
+
+  header: {
+    padding: theme.spacing.md,
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    marginTop: -theme.spacing.md,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    borderBottom: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  links: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+  },
+
+  linksInner: {
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+  },
+
+  footer: {
+    marginLeft: -theme.spacing.md,
+    marginRight: -theme.spacing.md,
+    marginBottom: -theme.spacing.md,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+
+  author: {
+    fontWeight: 500,
+    display: "block",
+    width: "100%",
+    padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+    color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+    fontSize: theme.fontSizes.sm,
+
+    "&:hover": {
+      backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[0],
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    },
+  },
+}));
+
+export const NavbarNested = () => {
+  const { classes } = useStyles();
+  const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
+
+  return (
+    <Navbar width={{ sm: 300 }} p="md" className={classes.navbar}>
+      <Navbar.Section className={classes.header}>
+        <Group position="apart">
+          <Title size={30} variant="gradient" gradient={{ from: "indigo", to: "cyan", deg: 45 }}>
+            Image Processing Question 100
+          </Title>
+        </Group>
+      </Navbar.Section>
+
+      <Navbar.Section grow className={classes.links} component={ScrollArea}>
+        <div className={classes.linksInner}>{links}</div>
+      </Navbar.Section>
+
+      <Navbar.Section className={classes.footer}>
+        <UnstyledButton
+          component="a"
+          href="https://github.com/Kitsuya0828"
+          className={classes.author}
+        >
+          <Group>
+            <IconBrandGithub />
+            <div>
+              <Text>Kitsuya Azuma</Text>
+              <Code sx={{ fontWeight: 700, color: "gray" }}>@Kitsuya0828</Code>
+            </div>
+          </Group>
+        </UnstyledButton>
+      </Navbar.Section>
+    </Navbar>
+  );
+};
